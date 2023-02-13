245920b2c29967f92e195538ff921e6ced526f10
RyanZheng <rzheng1232@gmail.com>
2023-02-13 14:12:34 -0500

    Added feedforward + Outputs motor agle vaules todashoard

--- testing-Imported/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ testing-Imported/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,12 +1,17 @@
 package frc.robot.subsystems;
+import org.opencv.core.Mat;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -22,37 +27,40 @@ public class ArmSubsystem extends SubsystemBase{
     // Initialize the goal point for the arm.
     private double[] goal = new double[2];
     private double currAngle = 0;
-    
+    public PIDController goToAngleShoulder = new PIDController(Constants.Swerve.sarmKP, Constants.Swerve.sarmKI, Constants.Swerve.sarmKD);
+    public PIDController goToAngleJoint = new PIDController(Constants.Swerve.jarmKP, Constants.Swerve.jarmKI, Constants.Swerve.jarmKD);
+    public ArmFeedforward Shoulderff = new ArmFeedforward(0, 0, 0, 0);
+    public ArmFeedforward Jointff = new ArmFeedforward(0, 0, 0, 0);
     public ArmSubsystem() {
         shoulderMotor = new CANSparkMax(armShoulder.rotMotorID, MotorType.kBrushless);
         jointMotor = new CANSparkMax(armJoint.rotMotorID, MotorType.kBrushless);
         EncoderShoulder = shoulderMotor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
         EncoderJoint = jointMotor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
-
     }
     public void moveToAngle(){
         Rotation2d[] angles = new Rotation2d[2];
         //get angles needed for each joint
         angles = this.getAngles(goal[0], goal[1]);
-        
-        PIDController goToAngleShoulder = new PIDController(Constants.Swerve.sarmKP, Constants.Swerve.sarmKI, Constants.Swerve.sarmKD);
-        PIDController goToAngleJoint = new PIDController(Constants.Swerve.jarmKP, Constants.Swerve.jarmKI, Constants.Swerve.jarmKD);
-
         double angle = angles[0].getRadians();
         goToAngleShoulder.setSetpoint(angle);
         goToAngleShoulder.setTolerance(0.05, 0.1);
         angle = angles[1].getRadians();
         goToAngleJoint.setSetpoint(angle);
         goToAngleJoint.setTolerance(0.05, 0.1);
+
         while (!goToAngleJoint.atSetpoint() && !goToAngleShoulder.atSetpoint()){
-            shoulderMotor.setVoltage(goToAngleShoulder.calculate(EncoderShoulder.getPosition() * Math.PI - Constants.Swerve.armShoulder.angleOffset.getRadians())*.12);
-            jointMotor.setVoltage(goToAngleJoint.calculate(EncoderJoint.getPosition()*Math.PI - Constants.Swerve.armShoulder.angleOffset.getRadians())*.12);
+            shoulderMotor.setVoltage((goToAngleShoulder.calculate(EncoderShoulder.getPosition() * Math.PI - Constants.Swerve.armShoulder.angleOffset.getRadians()) 
+                                    + Shoulderff.calculate(EncoderShoulder.getPosition() * Math.PI - Constants.Swerve.armShoulder.angleOffset.getRadians()
+                                    , EncoderShoulder.getVelocity()))*.12);
+            jointMotor.setVoltage((goToAngleJoint.calculate(EncoderJoint.getPosition()*Math.PI - Constants.Swerve.armJoint.angleOffset.getRadians())
+                                    + Jointff.calculate(EncoderJoint.getPosition() * Math.PI - Constants.Swerve.armJoint.angleOffset.getRadians()
+                                    , EncoderJoint.getVelocity()))*.12);
         }
     }
     /************************************************/
     public Rotation2d[] getAngles(double x, double y){
-        int AngleShoulder = 0;
-        int AngleJoint = 0;
+        double AngleShoulder = 0;
+        double AngleJoint = 0;
         Rotation2d[] angles = new Rotation2d[2];
         //Implement get angle code here Jouji
         double arm1Length = 1;
@@ -62,16 +70,7 @@ public class ArmSubsystem extends SubsystemBase{
         double theta2 = Math.acos(-(Math.pow(arm2Length,2)-(Math.pow(arm1Length,2)+Math.pow(hypotenuse,2)))/(2*arm1Length+hypotenuse));
         double j = Math.atan(y/x);
         double k = theta2-j;
-        AngleShoulder = (int)(90-k);
-
-
-
-
-
-        
-
-
-
+        AngleShoulder = (90-k);
         //convert angle to radians
         angles[1] = new Rotation2d(AngleShoulder);
         angles[2] = new Rotation2d(AngleJoint);
@@ -96,6 +95,12 @@ public class ArmSubsystem extends SubsystemBase{
             goal[0] = Constants.Swerve.HIGHGOAL[0];
             goal[1] = Constants.Swerve.HIGHGOAL[1];
         }
+    }
+    public void periodic(){
+        double joi = EncoderJoint.getPosition() * Math.PI;
+        double sho = EncoderShoulder.getPosition() * Math.PI;
+        SmartDashboard.putNumber("ShoulderMotor", sho);
+        SmartDashboard.putNumber("Joint", joi);
     }
     /*public void getNewPoint(){
         goal[0] = goal[0] + 0.05;
